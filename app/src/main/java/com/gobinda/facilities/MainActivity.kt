e2104969138bbd57671be2668f8364a76317d0f5
Gobinda Deb <gdeb089@gmail.com>
2019-05-31 10:12:21 +0530

    Added Navigation Drawer

--- app/src/main/java/com/gobinda/facilities/MainActivity.kt
+++ app/src/main/java/com/gobinda/facilities/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.gobinda.facilities
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
 import com.gobinda.facilities.di.ViewModelProviderFactory
 import com.gobinda.facilities.ui.FacilitiesViewModel
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 import javax.inject.Inject
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var factory : ViewModelProviderFactory
-     val model : FacilitiesViewModel by lazy {
+
+    val model : FacilitiesViewModel by lazy {
          ViewModelProviders.of(this, factory).get(FacilitiesViewModel::class.java)
      }
 
@@ -20,6 +25,17 @@ class MainActivity : FragmentActivity() {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
     }
 
     override fun onResume() {
